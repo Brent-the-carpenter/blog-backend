c69da801652e67177f3e633c5cd1b3468cd6545f
Brent-the-carpenter <beh337@gmail.com>
2024-07-14 23:07:47 -0400

    added a timer to force close the server in 10 seconds if it doesn't close on its own. added in a new route to retrieve all post by a user.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,28 @@
 import asyncHandler from "express-async-handler";
+import Post from "../models/post.js";
+import createError from "http-errors";
 
-asyncHandler(async (req, res, next) => {});
+const usersPosts = asyncHandler(async (req, res, next) => {
+  const userId = req.user._id;
+  const queryLimit = parseInt(req.query.limit, 10) || undefined;
+
+  try {
+    let query = Post.find({ author: userId }).sort({ likePost: -1 });
+
+    if (queryLimit) {
+      query = query.limit(queryLimit);
+    }
+
+    const postByUser = await query.exec();
+
+    if (!postByUser || postByUser.length === 0) {
+      return next(createError(404, "Posts not found"));
+    }
+
+    return res.status(200).json(postByUser);
+  } catch (error) {
+    return next(createError(500, "An error occurred"));
+  }
+});
+
+export { usersPosts };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,4 +1,7 @@
 import express from "express";
+import { usersPosts } from "../controllers/userController.js";
+import authenticate from "../middleware/authenticate.js";
 const router = express.Router();
 router.get("/");
+router.get("/posts", authenticate(), usersPosts);
 export default router;
